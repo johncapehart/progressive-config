92defbb36a1974430d69b64a444d1a2c28b11069
John Capehart <john.capehart@organicvalley.com>
2017-03-14 14:46:03 -0500

    0.1.16

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,6 @@ module.exports = function (grunt) {
 
   grunt.registerTask('release', 'Bump the version and publish to NPM.', function (type) {
     grunt.task.run([
-      'npm-contributors',
       'bump:' + (type || 'patch') + ':bump-only',
       'conventionalChangelog',
       'bump-commit',
